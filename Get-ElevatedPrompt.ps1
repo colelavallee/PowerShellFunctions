d5d3af94d402113471d51af1ec1b26e96cd3cb06
Cole Lavallee <Cole_Lavallee@waters.com>
2020-04-10 08:35:48 -0400

    formatted code & changed function name to approved pwsh verb

--- Get-ElevatedPrompt.ps1
+++ Get-ElevatedPrompt.ps1
@@ -1,17 +1,16 @@
 param([switch]$Elevated)
-function Check-Admin {
-$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-$currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+function Get-AdminStatus {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
-if ((Check-Admin) -eq $false)  {
-if ($elevated)
-{
-# could not elevate, quit
-}
+if ((Get-AdminStatus) -eq $false) {
+    if ($elevated) {
+        # could not elevate, quit
+    }
  
-else {
+    else {
  
-Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-}
-exit
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
 }
